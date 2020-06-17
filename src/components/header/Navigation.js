292cfd1bc50d3fc3cd33aebc12d7c87e6b82f18e
Carles <carles@crix.design>
2020-06-17 19:29:38 +0200

    styled header

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -3,18 +3,22 @@ import { Link } from "react-router-dom";
 
 import { NavigationMenu } from "../../configs/NavigationMenu";
 import "./Navigation.scss";
+import logoJordi from "../../assets/logos/JordiLogo_200.png";
 
 const Navigation = () => {
   return (
-    <ul className="header">
-      {NavigationMenu.map((tab) => {
-        return (
-          <li className="header header_item" key={tab.key}>
-            <Link to={tab.link}>{tab.name.toUpperCase()}</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div className="header">
+      <img src={logoJordi} alt="logo Jordi Arjó Terápias Psicólogo" />
+      <ul className="header_nav">
+        {NavigationMenu.map((tab) => {
+          return (
+            <li className="header header_item" key={tab.key}>
+              <Link to={tab.link}>{tab.name.toUpperCase()}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
